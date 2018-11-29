f36475fcfa22ce44d3d8abca509b0998fd6966e3
degwinthegreat <the.colledge.dropout@gmail.com>
2018-11-29 20:54:49 +0900

    javascript本格入門勉強中

---
+++ msg.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>JavaScript本格入門</title>
+    </head>
+    <body>
+        <script type="text/JavaScript">
+            msg = 'こんにちは、JavaScript！';
+            console.log(msg);
+        </script>
+    </body>
+</html>
